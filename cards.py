455318987ced05cda7cd9e1509d34ba794227a6d
Rodrigo Marquez <rod.m89@gmail.com>
2021-06-25 19:45:41 -0400

    first round

--- cards.py
+++ cards.py
@@ -10,9 +10,8 @@ class Units (Cards):
     self.power = power
     self.resistance = resistance
   
-
-  
-
+  def show_stats(self):
+    print(f'Name: {self.name}, Power: {self.power}, Resistance: {self.resistance}')
 
 
 class Effects(Cards):
@@ -21,5 +20,24 @@ class Effects(Cards):
     self.stat = stat
     self.magnitude = magnitude
 
-    def affect(unit):
-      pass
+  def affect(self, target):
+    if isinstance(target, Units) == False:
+      print('Must be played on a Unit card')
+      return
+    if self.stat == 'resistance':
+      target.resistance += self.magnitude
+    elif self.stat == 'power':
+      target.power += self.magnitude
+
+
+red_ninja = Units('Red Belt Ninja', 3, 3, 4)
+
+red_ninja.show_stats()
+
+# red_ninja.resistance +=3
+
+hard_algorithm = Effects('Hard Algorithm', 2, 'resistance', 3)
+
+hard_algorithm.affect(red_ninja)
+
+red_ninja.show_stats()
