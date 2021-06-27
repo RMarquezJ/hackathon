b3a1bf56188b33b43415dbbd02397852cf7f15ab
Rodrigo Marquez <rod.m89@gmail.com>
2021-06-26 20:25:34 -0400

    modified index.html

--- server.py
+++ server.py
@@ -11,31 +11,31 @@ red_ninja = Units('Red Belt Ninja', 3, 3, 4)
 black_ninja = Units('Black Belt Ninja', 4, 5, 4)
 yellow_ninja = Units('Yellow Belt Ninja', 4, 4 ,4)
 hard_algorithm = Effects('Hard Algorithm', 2, 'resistance', 3)
-not_managed_promise = Effects('Not Managed Promise', 1, 'resistance', -2)
+unhandled_promise = Effects('Unhandled Promise', 1, 'resistance', -2)
 pair_programming = Effects('Pair Programming', 3, 'power', 2)
 
 # red_ninja.play(1)
 # hard_algorithm.affect(red_ninja)
 # black_ninja.play(2)
-# not_managed_promise.affect(red_ninja)
+# unhandled_promise.affect(red_ninja)
 # pair_programming.affect(red_ninja)
 # red_ninja.attack(black_ninja)
 
-objetos = [
+list = [
 red_ninja,
 black_ninja,
 yellow_ninja,
 hard_algorithm,
-not_managed_promise,
+unhandled_promise,
 pair_programming
 ]
 
 player1 = Player('Player One')
 player2 = Player('player Two')
 
-player1.add_unit(red_ninja).add_unit(black_ninja).add_unit(yellow_ninja).add_effect(hard_algorithm).add_effect(not_managed_promise).add_effect(pair_programming)
+player1.add_unit(red_ninja).add_unit(black_ninja).add_unit(yellow_ninja).add_effect(hard_algorithm).add_effect(unhandled_promise).add_effect(pair_programming)
 
-player2.add_unit(red_ninja).add_unit(black_ninja).add_unit(yellow_ninja).add_effect(hard_algorithm).add_effect(not_managed_promise).add_effect(pair_programming)
+player2.add_unit(red_ninja).add_unit(black_ninja).add_unit(yellow_ninja).add_effect(hard_algorithm).add_effect(unhandled_promise).add_effect(pair_programming)
 
 def main():
   
@@ -80,18 +80,17 @@ def main():
           if which_target == '1':
           
             player.effects[which_effect].affect(player.units[which_unit])
-          
-          elif which_target =='2':
+            print(f'{player.units[which_unit].show_stats()}')
+
+          elif which_target == '2':
             
-            for i in range(len(player2.units)):
-              print(f'{i+1}: {player2.units[i].name}')
+            if player == player1:
+              for i in range(len(player2.units)):
+                print(f'{i+1}: {player2.units[i].name}')
             which_oponent = int(input('\n'))-1
-            # player.effects[which_effect].affect(player2.units[which_oponent])
-          
+            # player.effects[which_effect].affect(player2.units[which_oponent
 
           print(f'\nPlayer has used {player.effects[which_effect]} on {player.units[which_unit]}!')
-
-
 
       if choice == '2':
         print('\n--Unit cards--\t\t --Effect cards--\n')
@@ -101,13 +100,11 @@ def main():
       if choice =='3':
         break
 
+@app.route("/")
+def hello_world():
+    return render_template('index.html', list=list)
 
 
-# @app.route("/")
-# def hello_world():
-#     return render_template('index.html', objetos=objetos)
-
-
-# app.run(debug=True)
+app.run(debug=True)
 
 main()
